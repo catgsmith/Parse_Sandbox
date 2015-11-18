b4ca0b97d26e76c3331353650d7bff7e0e8b5974
catgsmith <catgsmith@hotmail.com>
2015-11-18 11:53:51 +0000

    list Post from Parse

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@
     	Parse.initialize(PARSE_CREDENTIALS.APP_ID, PARSE_CREDENTIALS.JAVASCRIPT_KEY);
     });
 
-    myApp.controller('myController', function ($scope) {
+    myApp.controller('myController', function ($scope, $q) {
 
 		var Post = Parse.Object.extend("Post");
 
@@ -20,14 +20,48 @@
 	        newPost.set("content", content);
 
 			newPost.save({
-				success: function(object) {
-					$(".success").show();
-				},
-				error: function(error) {
-					console.log("~Log Error: " + error.message);
-					$(".error").show();
-				}
+			    success: function(object) {
+			        $(".success").show();
+			        getPosts().then(function(posts) {
+			            $scope.posts = posts;
+			        });
+			    },
+			    error: function(error) {
+			        console.log("~Log Error: " + error.message);
+			        $(".error").show();
+			    }
 			});
+
     	});
-	});
+
+		function getPosts() {
+	 	  var defer = $q.defer();
+		  var posts = [];
+
+	      var query = new Parse.Query('Post');
+	      query.find({
+	          success: function(results) { // return an array of parse objects
+	              
+
+	              angular.forEach(results, function(parse_post) {
+	                  var post = {};
+	                  post.title = parse_post.get("title");
+	                  post.content = parse_post.get("content");
+	                  posts.push(post);
+	              });
+	              
+	              defer.resolve(posts);
+	          },
+	          error: function(error) {
+	              console.log("Query Error: " + error.message);
+	              defer.reject(error);
+	          }
+	      });
+	      return defer.promise;
+	  	}
+
+	  	getPosts().then(function(posts){
+	  		$scope.posts = posts;	  		
+	  	});
+	});  // controller
 }());	
